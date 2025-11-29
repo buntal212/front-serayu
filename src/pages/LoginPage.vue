2c1f7bb61748567a462c8effa54f3e2120f48a25
kentangkotak <ariezlnd69@gmail.com>
2025-11-29 09:52:06 +0700

    xxx

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -40,7 +40,7 @@
 
 <script setup>
 import { useQuasar } from 'quasar'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { api } from 'src/boot/axios'
 
@@ -85,19 +85,19 @@ async function onSubmit() {
       $q.notify({ type: 'positive', message: 'Login berhasil!' })
       router.push('/')
 
-      // setTimeout(() => {
-      //   if (window.deferredPrompt) {
-      //     console.log('🔥 Menampilkan prompt Install App...')
-      //     window.deferredPrompt.prompt()
+      setTimeout(() => {
+        if (window.deferredPrompt) {
+          console.log('🔥 Menampilkan prompt Install App...')
+          window.deferredPrompt.prompt()
 
-      //     window.deferredPrompt.userChoice.then((choice) => {
-      //       console.log('User pilihan:', choice.outcome)
-      //       window.deferredPrompt = null
-      //     })
-      //   } else {
-      //     console.log('❗ A2HS belum tersedia')
-      //   }
-      // }, 500)
+          window.deferredPrompt.userChoice.then((choice) => {
+            console.log('User pilihan:', choice.outcome)
+            window.deferredPrompt = null
+          })
+        } else {
+          console.log('❗ A2HS belum tersedia')
+        }
+      }, 100)
     }
   } catch (error) {
     console.error(error)
@@ -106,21 +106,21 @@ async function onSubmit() {
     loading.value = false
   }
 }
-onMounted(() => {
-  setTimeout(() => {
-    if (window.deferredPrompt) {
-      console.log('🔥 Menampilkan prompt Install App...')
-      window.deferredPrompt.prompt()
+// onMounted(() => {
+//   setTimeout(() => {
+//     if (window.deferredPrompt) {
+//       console.log('🔥 Menampilkan prompt Install App...')
+//       window.deferredPrompt.prompt()
 
-      window.deferredPrompt.userChoice.then((choice) => {
-        console.log('User pilihan:', choice.outcome)
-        window.deferredPrompt = null
-      })
-    } else {
-      console.log('❗ A2HS belum tersedia')
-    }
-  }, 500)
-})
+//       window.deferredPrompt.userChoice.then((choice) => {
+//         console.log('User pilihan:', choice.outcome)
+//         window.deferredPrompt = null
+//       })
+//     } else {
+//       console.log('❗ A2HS belum tersedia')
+//     }
+//   }, 500)
+// })
 </script>
 
 <style scoped>
